85fa60c76a84e0b734bf937fb61d4ae617268cf3
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-12-05 16:12:12 +0530

    Created Navbar and Login form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
